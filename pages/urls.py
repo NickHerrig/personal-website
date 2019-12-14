50f1ba467ad6492684aefde91473daff14c5e1b8
nickherrig <neherrig@gmail.com>
2019-12-14 10:38:02 -0600

    Deploy /login page bulma/html/images

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('login', views.LoginPageView.as_view(), name='login'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+
+class LoginPageView(TemplateView):
+    template_name = 'pages/login.html'
