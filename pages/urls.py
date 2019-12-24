8505a07d4fa0ae5ba13a735fbd909df84e52367a
nickherrig <neherrig@gmail.com>
2019-12-24 14:22:30 -0600

    Change layout for personal website

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='books'),
+    path('', views.HomePageView.as_view(), name='home'),
     path('login', views.LoginPageView.as_view(), name='login'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'pages/books.html'
+    template_name = 'pages/home.html'
 
 
 class LoginPageView(TemplateView):
