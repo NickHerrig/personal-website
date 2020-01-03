6ced18917986ed7609442dbe68685dc71db4a56d
nickherrig <neherrig@gmail.com>
2020-01-02 18:43:08 -0600

    Add url, settings, and base.html file for navbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'essays.apps.EssaysConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
