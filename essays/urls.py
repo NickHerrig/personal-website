a7034c0d1ed446859459074852098efec6a9731f
nickherrig <neherrig@gmail.com>
2020-01-02 20:17:14 -0600

    Add base template for navbar

--- essays/urls.py
+++ essays/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='essays'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<slug:slug>/', views.DetailView.as_view(), name='detail'),
 ]
--- essays/views.py
+++ essays/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello from my essays application")
+from .models import Essay
 
+class IndexView(generic.ListView):
+    template_name = 'essays/index.html'
+    context_object_name = 'latest_essays_list'
+    
+    def get_queryset(self):
+        """Return the 4 most recently published blogs."""
+        return Essay.objects.order_by('-pub_date')[:4]
+
+class DetailView(generic.DetailView):
+    model = Essay
+    template_name = 'essays/detail.html'
 
